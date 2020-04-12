304409845f8a52c542f2a135019c4e2a5dcef25b
hong <gmm117@naver.com>
2020-04-12 15:38:00 +0900

    redux conntect add

--- learn-redux/src/containers/CounterContainer.js
+++ learn-redux/src/containers/CounterContainer.js
@@ -1,48 +1,51 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
 import Counter from '../components/Counter';
-import { useSelector, useDispatch, shallowEqual } from 'react-redux';
+import { connect } from 'react-redux';
 import { increase, decrease, setDiff } from '../modules/counter';
 
-function CounterContainer() {
-    //state => store.getState() -> 리덕스의 현재상태
-    // 상태를 조회하는 hook
-    // useSelector에서 새로운 object를 만들기때문에 rerendering 발생
-    // shallowEqual 얕은 비교만 가능
-    // const { number, diff } = useSelector(state => ({
-    //     number: state.counter.number,
-    //     diff: state.counter.diff
-    // }));
-    // const { number } = useSelector(state => state.counter.number);
-    // const { diff } = useSelector(state => state.counter.diff);
-    // const { number, diff } = useSelector(state => ({
-    //     number: state.counter.number,
-    //     diff: state.counter.diff
-    // }), 
-    // (left, right) => {
-    //     return left.diff === right.diff && left.number === right.number
-    // });
-    const { number, diff } = useSelector(state => ({
-        number: state.counter.number,
-        diff: state.counter.diff
-        }), 
-        shallowEqual
-    );
-
-    const dispatch = useDispatch();
-
-    const onIncrease = () => dispatch(increase());
-    const onDecrease = () => dispatch(decrease());
-    const onSetDiff = diff => dispatch(setDiff(diff));
+function CounterContainer({
+    number,
+    diff,
+    increase,
+    decrease,
+    setDiff
+}) {
+    
 
     return(
         <Counter 
             number={number}
             diff={diff}
-            onIncrease={onIncrease}
-            onDecrease={onDecrease}
-            onSetDiff={onSetDiff}
+            onIncrease={increase}
+            onDecrease={decrease}
+            onSetDiff={setDiff}
         />
     )
 }
 
-export default CounterContainer;
+const mapStateToProps = (state) => ({
+    number: state.counter.number,
+    diff: state.counter.diff
+});
+
+// const mapDispatchToProps = dispatch => ({
+//     onIncrease: () => dispatch(increase()),
+//     onDecrease: () => dispatch(decrease()),
+//     setDiff: (diff) => dispatch(setDiff(diff))
+// });
+
+// const mapDispatchToProps = dispatch => 
+//     bindActionCreators({
+//         increase,
+//         decrease,
+//         setDiff       
+//     }, dispatch);
+// 객체로 만들경우 내부적으로 bindActionCreators호출되서 처리됨
+const mapDispatchToProps = {
+        increase,
+        decrease,
+        setDiff       
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
--- learn-redux/src/containers/TodosContainer.js
+++ learn-redux/src/containers/TodosContainer.js
@@ -1,14 +1,12 @@
 import React, { useCallback } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { connect } from 'react-redux';
 import Todos from '../components/Todos';
 import { addTodo, toggleTodo } from '../modules/todos';
 
-function TodosContainer() {
-    const todos = useSelector(state => state.todos);
-    const dispatch = useDispatch();
-
-    const onCreate = useCallback(text => dispatch(addTodo(text)), [dispatch]);
-    const onToggle = useCallback(id => dispatch(toggleTodo(id)), [dispatch]);
+function TodosContainer({ todos, addTodo, toggleTodo }) {
+    
+    const onCreate = useCallback(text => addTodo(text), [addTodo]);
+    const onToggle = useCallback(id => toggleTodo(id), [toggleTodo]);
 
     return(
         <Todos
@@ -19,4 +17,21 @@ function TodosContainer() {
     )
 }
 
-export default TodosContainer;
+// const mapStateToProps = (state) => ({
+//     todos: state.todos 
+// });
+
+// const mapDispatchToProps = {
+//     addTodo,
+//     toggleTodo      
+// };
+
+
+export default connect(
+    (state) => ({
+    todos: state.todos }), 
+    {
+        addTodo,
+        toggleTodo      
+    }
+)(TodosContainer);
